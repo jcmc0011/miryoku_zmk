f7ab61544f2cac140af0f1e97f48cd8be331df62
jcmc0011 <jcmc0011@gmail.com>
2024-10-23 18:50:27 +0200

    Update custom_config.h

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -4,7 +4,7 @@
 
 #define MIRYOKU_LAYER_BASE \
 &kp Q,             &kp W,             &kp E,             &kp R,             &kp T,             &kp Y,             &kp U,             &kp I,             &kp O,             &kp P,             \
-U_MT(LSHFT, A),     U_MT(LGUI, S),     U_MT(LCTRL, D),    U_MT(LALT, F),    &kp G,             &kp H,             U_MT(LALT, J),    U_MT(LCTRL, K),    U_MT(LGUI, L),     U_MT(LSHFT, SEMI),   \
+U_MT(LSHFT, A),    U_MT(LALT, S),     U_MT(LGUI, D),     U_MT(LCTRL, F),    &kp G,             &kp H,             U_MT(LCTRL, J),    U_MT(LGUI, K),     U_MT(LALT, L),     U_MT(LSHFT, SEMI),   \
 U_LT(U_BUTTON, Z), U_MT(RALT, X),     &kp C,             &kp V,             &kp B,             &kp N,             &kp M,             &kp COMMA,         U_MT(RALT, DOT),   U_LT(U_BUTTON, SLASH),\
 U_NP,              U_NP,              U_LT(U_MEDIA, ESC),U_LT(U_NAV, SPACE),U_LT(U_MOUSE, TAB),U_LT(U_SYM, RET),  U_LT(U_NUM, BSPC), U_LT(U_FUN, DEL),  U_NP,              U_NP
 
