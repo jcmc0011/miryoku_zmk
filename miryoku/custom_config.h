64cc9ed7bddd89cb214504dd4396dd2d262f8c91
jcmc0011 <jcmc0011@gmail.com>
2024-11-12 23:30:33 +0100

    Update custom_config.h

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -9,6 +9,17 @@ MIRYOKU_X(NUM,    "Num") \
 MIRYOKU_X(SYM,    "Sym") \
 MIRYOKU_X(FUN,    "Fun")
 
+#define U_BASE   0
+#define U_EXTRA  1
+#define U_TAP    2
+#define U_BUTTON 3
+#define U_NAV    4
+#define U_MOUSE  5
+#define U_MEDIA  6
+#define U_NUM    7
+#define U_SYM    8
+#define U_FUN    9
+
 #define MIRYOKU_LAYOUTMAPPING_LILY58( \
   K00, K01, K02, K03, K04, K05,              K06, K07, K08, K09, K10, K11, \
   K12, K13, K14, K15, K16, K17,              K18, K19, K20, K21, K22, K23, \
